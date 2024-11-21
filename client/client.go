b13e248e0ef8065c10c38bda0ede8bbe4eca6c75
Chester-232 <jairadizon558@gmail.com>
2024-11-21 09:53:06 +0800

    ehh

--- client/client.go
+++ client/client.go
@@ -17,7 +17,7 @@ func Run() {
 	var IP string;
 	fmt.Print("Enter the server IP: ")
 	fmt.Scan(&IP);
-	conn, err := net.Dial("tcp", IP+ ":8080");
+	conn, err := net.Dial("tcp", IP + ":8080");
 	utils.Checker("Error connecting to the server: ", err);
 	defer conn.Close()
 
