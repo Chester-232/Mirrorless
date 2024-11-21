e417f73a495c5aa1c0bceb3444fd48e4e51538ed
Chester-232 <jairadizon558@gmail.com>
2024-11-21 08:55:34 +0800

    added the client

--- client/client.go
+++ client/client.go
@@ -1,13 +1,59 @@
 package client
 
 import (
+	"bufio"
+	"fmt"
+	"io"
 	"net"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
 
 	"github.com/Chester-232/Mirrorless/utils"
 )
 
-func main() {
+func Run() {
 	conn, err := net.Dial("tcp", ":8080");
 	utils.Checker("Error connecting to the server: ", err);
 	defer conn.Close()
+
+	fmt.Println("Connected to server.");
+	destDir := "./destination";
+	os.MkdirAll(destDir, 0755);
+
+	for {
+		reader := bufio.NewReader(conn);
+
+		meta, err := reader.ReadString('\n');
+		if err != nil {
+			if err == io.EOF {
+				fmt.Println("Connection closed by the server.");
+				return;
+			}
+			fmt.Println("Error reading metadata: ", err);
+			return;
+		}
+		meta = strings.TrimSpace(meta);
+		parts := strings.Split(meta, "|");
+		if len(parts) != 2 {
+			fmt.Println("Invalid metadata format.")
+			continue;
+		}
+		fileName := parts[0];
+		fileSize, _ := strconv.ParseInt(parts[1], 10, 64);
+
+		destPath := filepath.Join(destDir, fileName);
+		outFile, err := os.Create(destPath)
+		if err != nil {
+			fmt.Println("Error creating file: ", err);
+			return;
+		}
+		written, err := io.CopyN(outFile, reader, fileSize);
+		outFile.Close()
+		if err != nil && err != io.EOF {
+			fmt.Println("Error writing File: ", err)
+		}
+		fmt.Printf("Received file %s (%d bytes)\n", fileName, written);
+	}
 }
--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "github.com/Chester-232/Mirrorless/server"
+import (
+	"fmt"
+
+	"github.com/Chester-232/Mirrorless/client"
+	"github.com/Chester-232/Mirrorless/server"
+)
 
 func main() {
-	server.Run();
+	var choice int;
+	
+	mainloop:
+	for {
+		fmt.Print("Which side are you running?\n1. Server\n2.Client\n3.Quit\n->");
+		fmt.Scan(&choice);
+		switch choice {
+		case 1:
+			server.Run()
+		case 2:
+			client.Run()
+		case 3:
+			break mainloop;
+		default:
+			fmt.Println("Invalid choice.")
+		}
+	}
 }
